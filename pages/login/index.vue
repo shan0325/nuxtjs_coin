d91e6ae669c8d7c6e4c06131b72ae7224f3eec74
shan0325 <shan0325@naver.com>
2021-06-21 00:02:38 +0900

    add login page

---
+++ pages/login/index.vue
@@ -0,0 +1,113 @@
+<template>
+	<v-main>
+		<v-container fluid fill-height>
+			<v-layout align-center justify-center>
+				<v-flex xs12 sm8 md4>
+					<v-card>
+						<v-toolbar color="primary">
+							<v-toolbar-title>Admin Panel</v-toolbar-title>
+							<v-spacer />
+						</v-toolbar>
+						<v-card-text>
+							<v-form>
+								<v-text-field
+									v-model="userId"
+									prepend-icon="mdi-alien"
+									:rules="[
+										() =>
+											!!userId ||
+											'아이디는 필수 입력입니다.',
+									]"
+									label="아이디"
+									required
+									autofocus
+								></v-text-field>
+								<v-text-field
+									v-model="password"
+									prepend-icon="mdi-lock"
+									:rules="[
+										() =>
+											!!password ||
+											'비밀번호는 필수 입력입니다.',
+									]"
+									:append-icon="
+										showPassword ? 'mdi-eye-off' : 'mdi-eye'
+									"
+									:type="showPassword ? 'text' : 'password'"
+									label="비밀번호"
+									counter
+									required
+									@keydown.enter="doLogin"
+									@click:append="showPassword = !showPassword"
+								></v-text-field>
+							</v-form>
+						</v-card-text>
+						<v-card-actions>
+							<v-btn
+								block
+								depressed
+								large
+								color="primary"
+								@click="doLogin"
+								>Login</v-btn
+							>
+						</v-card-actions>
+						<v-snackbar
+							v-model="snackbar"
+							:color="color"
+							:top="true"
+						>
+							{{ errorMessages }}
+							<v-btn dark @click="snackbar = false">
+								Close
+							</v-btn>
+						</v-snackbar>
+					</v-card>
+				</v-flex>
+			</v-layout>
+		</v-container>
+	</v-main>
+</template>
+
+<script>
+export default {
+	layout: 'login',
+	data() {
+		return {
+			userId: '',
+			password: '',
+			errorMessages: 'Incorrect login info',
+			snackbar: false,
+			color: 'general',
+			showPassword: false,
+		};
+	},
+	methods: {
+		doLogin() {
+			const userId = this.userId;
+			const password = this.password;
+
+			if (userId === '') {
+				this.errorMessages = '아이디는 필수 입력입니다.';
+				this.snackbar = true;
+				return;
+			}
+			if (password === '') {
+				this.errorMessages = '비밀번호는 필수 입력입니다.';
+				this.snackbar = true;
+			}
+
+			// this.$store
+			// 	.dispatch('login', { userId, password })
+			// 	.then(response => {
+			// 		this.$router.push(common.ROOT_PATH + '/dashboard');
+			// 	})
+			// 	.catch(error => {
+			// 		console.log(error.response.data.error_description);
+			// 		this.errorMessages = '로그인 정보를 다시 확인해주세요.';
+			// 		this.snackbar = true;
+			// 	});
+		},
+	},
+};
+</script>
