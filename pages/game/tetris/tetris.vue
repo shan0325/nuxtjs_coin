85787ec37d17435e46fd592c8df5d50bbefc2710
shan0325 <shan0325@naver.com>
2021-09-19 00:57:50 +0900

    h

--- pages/game/tetris/tetris.vue
+++ pages/game/tetris/tetris.vue
@@ -18,12 +18,16 @@ export default {
 			},
 			shapes: [
 				{
+					seq: 1,
 					type: 'I',
+					color: '#f44336',
 					startPosX: 4,
 					mat: [[[1], [1], [1], [1]], [[1, 1, 1, 1]]],
 				},
 				{
+					seq: 2,
 					type: 'J',
+					color: '#e91e63',
 					startPosX: 4,
 					mat: [
 						[
@@ -47,7 +51,9 @@ export default {
 					],
 				},
 				{
+					seq: 3,
 					type: 'L',
+					color: '#9c27b0',
 					startPosX: 4,
 					mat: [
 						[
@@ -71,7 +77,9 @@ export default {
 					],
 				},
 				{
+					seq: 4,
 					type: 'O',
+					color: '#00bcd4',
 					startPosX: 4,
 					mat: [
 						[
@@ -81,7 +89,9 @@ export default {
 					],
 				},
 				{
+					seq: 5,
 					type: 'T',
+					color: '#009688',
 					startPosX: 4,
 					mat: [
 						[
@@ -105,7 +115,9 @@ export default {
 					],
 				},
 				{
+					seq: 6,
 					type: 'Z',
+					color: '#cddc39',
 					startPosX: 4,
 					mat: [
 						[
@@ -120,7 +132,9 @@ export default {
 					],
 				},
 				{
+					seq: 7,
 					type: 'S',
+					color: '#795548',
 					startPosX: 4,
 					mat: [
 						[
@@ -141,10 +155,14 @@ export default {
 				posX: 0,
 				posY: 0,
 				direction: 0,
+				isBottomCollisioned: false,
+				isRightCollisioned: false,
+				isLeftCollisioned: false,
+				isUpCollisioned: false,
 			},
 			speed: 1,
 			board: null,
-			beforeBoard: null,
+			buildedBoard: null,
 			reqAni: null,
 		};
 	},
@@ -164,29 +182,55 @@ export default {
 			if (e.keyCode === 39) {
 				// right
 				const matrix = this.shape.obj.mat[this.shape.direction];
-				if (this.shape.posX + matrix[0].length < this.block.cols) {
+				this.checkRightEventCollision();
+				if (
+					this.shape.posX + matrix[0].length < this.block.cols &&
+					!this.shape.isRightCollisioned
+				) {
 					this.shape.posX += 1;
 				}
 			} else if (e.keyCode === 37) {
 				// left
-				if (this.shape.posX > 0) {
+				this.checkLeftEventCollision();
+				if (this.shape.posX > 0 && !this.shape.isLeftCollisioned) {
 					this.shape.posX -= 1;
 				}
 			} else if (e.keyCode === 38) {
 				// up
-				if (this.shape.direction >= this.shape.obj.mat.length - 1) {
-					this.shape.direction = 0;
-				} else {
-					this.shape.direction += 1;
+				let nextDirection = 0;
+				if (this.shape.direction < this.shape.obj.mat.length - 1) {
+					nextDirection = this.shape.direction + 1;
+				}
+
+				const matrix = this.shape.obj.mat[nextDirection];
+				this.checkUpEventCollision(nextDirection);
+				if (
+					this.shape.posX + matrix[0].length <= this.block.cols &&
+					this.shape.posX >= 0 &&
+					!this.shape.isUpCollisioned
+				) {
+					this.shape.direction = nextDirection;
+
+					const matrix = this.shape.obj.mat[this.shape.direction];
+					if (this.shape.posX + matrix[0].length > this.block.cols) {
+						this.shape.posX = this.block.cols - matrix[0].length;
+					}
+					if (this.shape.posY + matrix.length > this.block.rows) {
+						this.shape.posY = this.block.rows - matrix.length;
+					}
 				}
 			} else if (e.keyCode === 40) {
 				// down
 				this.speed = 10;
+			} else if (e.keyCode === 32) {
+				// space
+				const matrix = this.shape.obj.mat[this.shape.direction];
+				this.shape.posY = this.block.rows - matrix.length;
 			}
 		},
 		keyUpHandler(e) {
 			if (e.keyCode === 40) {
-				// up
+				// down
 				this.speed = 1;
 			}
 		},
@@ -199,42 +243,161 @@ export default {
 			this.start();
 
 			// this.reqAni = requestAnimationFrame(this.draw);
-			this.reqAni = setTimeout(this.draw, 500 / this.speed);
+			this.reqAni = setTimeout(this.draw, 300 / this.speed);
 		},
 		start() {
-			this.makeShape();
+			this.makeShape(); // 랜덤 도형 생성
+			this.initBoard(); // 초기화
+			this.checkBottomCollision(); // 아래 충돌 확인
+			this.setBoard(); // 보드 셋팅
 
-			for (let i = 0; i < this.board.length; i++) {
-				this.board[i].fill(0);
-			}
-
-			if (this.beforeBoard) {
-				this.cloneBoard(this.beforeBoard, this.board);
-			}
-
-			const matrix = this.shape.obj.mat[this.shape.direction];
-			for (let i = 0; i < matrix.length; i++) {
-				for (let j = 0; j < matrix[i].length; j++) {
-					if (matrix[i][j] === 1) {
-						if (this.shape.posY + i < this.board.length) {
-							this.board[this.shape.posY + i][
-								this.shape.posX + j
-							] = 1;
-						}
-					}
-				}
-			}
-
-			if (this.shape.posY + matrix.length >= this.block.rows) {
-				this.beforeBoard = JSON.parse(JSON.stringify(this.board));
+			if (this.shape.isBottomCollisioned) {
+				this.buildedBoard = JSON.parse(JSON.stringify(this.board));
 				this.shape.isMake = false;
 				this.shape.posY = 0;
 			} else {
 				this.shape.posY += 1;
 			}
 
-			this.printBoard(this.board);
+			// this.printBoard(this.board);
 			this.drawShape();
+		},
+		setBoard() {
+			const matrix = this.shape.obj.mat[this.shape.direction];
+			for (let i = 0; i < matrix.length; i++) {
+				for (let j = 0; j < matrix[i].length; j++) {
+					if (matrix[i][j] === 1) {
+						if (
+							this.shape.posY + matrix.length <=
+							this.block.rows
+						) {
+							this.board[this.shape.posY + i][
+								this.shape.posX + j
+							] = this.shape.obj.seq;
+						}
+					}
+				}
+			}
+		},
+		checkLeftEventCollision() {
+			const matrix = this.shape.obj.mat[this.shape.direction];
+			const nextLeftPosX = this.shape.posX - 1;
+
+			this.shape.isLeftCollisioned = false;
+
+			if (this.buildedBoard) {
+				for (let i = 0; i < matrix.length; i++) {
+					for (let j = 0; j < matrix[i].length; j++) {
+						if (matrix[i][j] === 1) {
+							// 왼쪽 도형 충돌 확인
+							if (
+								this.buildedBoard[this.shape.posY + i][
+									nextLeftPosX + j
+								] > 0
+							) {
+								console.log('왼쪽 도형 충돌');
+								this.shape.isLeftCollisioned = true;
+								return;
+							}
+						}
+					}
+				}
+			}
+		},
+		checkRightEventCollision() {
+			const matrix = this.shape.obj.mat[this.shape.direction];
+			const nextRightPosX = this.shape.posX + 1;
+
+			this.shape.isRightCollisioned = false;
+
+			if (this.buildedBoard) {
+				for (let i = 0; i < matrix.length; i++) {
+					for (let j = 0; j < matrix[i].length; j++) {
+						if (matrix[i][j] === 1) {
+							// 오른쪽 도형 충돌 확인
+							if (
+								this.buildedBoard[this.shape.posY + i][
+									nextRightPosX + j
+								] > 0
+							) {
+								console.log('오른쪽 도형 충돌');
+								this.shape.isRightCollisioned = true;
+								return;
+							}
+						}
+					}
+				}
+			}
+		},
+		checkUpEventCollision(nextDirection) {
+			this.shape.isUpCollisioned = false;
+			const nextMatrix = this.shape.obj.mat[nextDirection];
+
+			if (
+				this.shape.posY + (nextMatrix.length - 1) >
+				this.block.rows - 1
+			) {
+				console.log('도형 변경 바닥 충돌');
+				this.shape.isUpCollisioned = true;
+				return;
+			}
+
+			if (this.buildedBoard) {
+				for (let i = 0; i < nextMatrix.length; i++) {
+					for (let j = 0; j < nextMatrix[i].length; j++) {
+						if (nextMatrix[i][j] === 1) {
+							// 도형 변경 충돌 확인
+							if (
+								this.buildedBoard[this.shape.posY + i][
+									this.shape.posX + j
+								] > 0
+							) {
+								console.log('도형 변경 충돌');
+								this.shape.isUpCollisioned = true;
+								return;
+							}
+						}
+					}
+				}
+			}
+		},
+		checkBottomCollision() {
+			const matrix = this.shape.obj.mat[this.shape.direction];
+			const nextPosY = this.shape.posY + 1;
+
+			this.shape.isBottomCollisioned = false;
+			if (nextPosY + (matrix.length - 1) > this.block.rows - 1) {
+				console.log('바닥 충돌');
+				this.shape.isBottomCollisioned = true;
+				return;
+			}
+
+			if (this.buildedBoard) {
+				for (let i = 0; i < matrix.length; i++) {
+					for (let j = 0; j < matrix[i].length; j++) {
+						if (matrix[i][j] === 1) {
+							// 아래 도형 충돌 확인
+							if (
+								this.board[nextPosY + i][this.shape.posX + j] >
+								0
+							) {
+								console.log('아래 도형 충돌');
+								this.shape.isBottomCollisioned = true;
+								return;
+							}
+						}
+					}
+				}
+			}
+		},
+		initBoard() {
+			for (let i = 0; i < this.board.length; i++) {
+				this.board[i].fill(0);
+			}
+
+			if (this.buildedBoard) {
+				this.cloneBoard(this.buildedBoard, this.board);
+			}
 		},
 		makeShape() {
 			if (!this.shape.isMake) {
@@ -243,15 +406,16 @@ export default {
 				this.shape.posX = this.shape.obj.startPosX;
 				this.shape.isMake = true;
 				this.shape.direction = 0;
-				console.log(this.shape.obj);
+				// console.log(this.shape.obj);
 			}
 		},
 		drawShape() {
 			for (let i = 0; i < this.board.length; i++) {
 				for (let j = 0; j < this.board[i].length; j++) {
-					if (this.board[i][j] === 1) {
+					if (this.board[i][j] > 0) {
 						this.ctx.beginPath();
-						this.ctx.fillStyle = '#999';
+						this.ctx.fillStyle =
+							this.shapes[this.board[i][j] - 1].color;
 						this.ctx.fillRect(
 							j * this.block.size,
 							i * this.block.size,
